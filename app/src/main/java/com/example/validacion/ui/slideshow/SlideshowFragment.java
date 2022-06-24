b0b0d4c8e221feba105e8b10c54eb1730875f57c
BrandonAlemanE11 <105066717+BrandonAlemanE11@users.noreply.github.com>
2022-06-24 11:36:07 -0500

    avance

--- app/src/main/java/com/example/validacion/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/validacion/ui/slideshow/SlideshowFragment.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.validacion.databinding.FragmentSlideshowBinding;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SlideshowFragment extends Fragment {
 
@@ -26,7 +37,50 @@ public class SlideshowFragment extends Fragment {
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final Button btnValidar = binding.btnValidar;
+        btnValidar.setOnClickListener(new View.OnClickListener() {
+            final EditText etnCode = binding.etnCode;
+            final EditText edcEmail = binding.edcEmail;
+            final HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+            @Override
+            public void onClick(View view) {
+                String email = edcEmail.getText().toString().trim();
+                String Code = etnCode.getText().toString().trim();
+                if(!email.isEmpty() && !Code.isEmpty()){
+
+                }
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+                final OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+                httpClient.addInterceptor(logging);
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://eyiogthd.lucusvirtual.es/api/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(httpClient.build())
+                        .build();
+                validarApi val = retrofit.create(validarApi.class);
+                Call<validar> call = val.VALIDAR_CALL(email, Code);
+                call.enqueue(new Callback<validar>() {
+                    @Override
+                    public void onResponse(Call<validar> call, Response<validar> response) {
+                        if(response.isSuccessful() && response.body() != null){
+                            edcEmail.getText().clear();
+                            etnCode.getText().clear();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<validar> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
         return root;
+
+
+
     }
 
     @Override
